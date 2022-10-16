d2e0919411c4533319891a126e08550a1dec0e12
Reznov <reznov03048@gmail.com>
2022-10-16 19:37:40 +0900

    Spring의 @Configuration, @Bean 을 사용해서 DI 해보기

--- inflearn/src/main/java/SpringExample/inflearn/OrderApp.java
+++ inflearn/src/main/java/SpringExample/inflearn/OrderApp.java
@@ -5,14 +5,19 @@ import SpringExample.inflearn.member.Member;
 import SpringExample.inflearn.member.MemberService;
 import SpringExample.inflearn.order.Order;
 import SpringExample.inflearn.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
