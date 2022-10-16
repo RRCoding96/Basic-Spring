d291e2be5f74ab968fd418a2e3645d7fb19e41eb
Reznov <reznov03048@gmail.com>
2022-10-16 19:21:31 +0900

    Spring 적용: AppConfiguration

--- inflearn/src/main/java/SpringExample/inflearn/AppConfig.java
+++ inflearn/src/main/java/SpringExample/inflearn/AppConfig.java
@@ -8,21 +8,28 @@ import SpringExample.inflearn.member.MemberServiceImpl;
 import SpringExample.inflearn.member.MemoryMemberRepository;
 import SpringExample.inflearn.order.OrderService;
 import SpringExample.inflearn.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
