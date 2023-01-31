151b36ea973f0763ce0f005b27da4679f42175ee
Reznov <reznov03048@gmail.com>
2023-01-31 22:04:45 +0900

    feat: Component를 통해 의존성을 주입하는 과정 test 작성

--- inflearn/src/main/java/SpringExample/inflearn/discount/RateDiscountPolicy.java
+++ inflearn/src/main/java/SpringExample/inflearn/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package SpringExample.inflearn.discount;
 
 import SpringExample.inflearn.member.Grade;
 import SpringExample.inflearn.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
