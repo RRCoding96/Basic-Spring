f3f56954e4bb56818f2be8e131e38cc2a9cedf0a
Reznov <reznov03048@gmail.com>
2023-02-02 21:43:46 +0900

    feat: Lombok 라이브러리 추가 후 생성자 생략

--- inflearn/src/main/java/SpringExample/inflearn/order/OrderServiceImpl.java
+++ inflearn/src/main/java/SpringExample/inflearn/order/OrderServiceImpl.java
@@ -6,20 +6,23 @@ import SpringExample.inflearn.discount.RateDiscountPolicy;
 import SpringExample.inflearn.member.Member;
 import SpringExample.inflearn.member.MemberRepository;
 import SpringExample.inflearn.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // lombok : @RequiredArgsConstructor가 만들어 줌
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
